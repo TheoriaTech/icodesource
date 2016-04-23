0965291266d4cf3044f30c0a13ed9d7ed41a4141
Colin Thomas <Colin Thomas>
2016-04-23 12:32:18 +0530

    Projects created

---
+++ Skunkworks.Ics.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Skunkworks.Ics.Web.Startup))]
+namespace Skunkworks.Ics.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
